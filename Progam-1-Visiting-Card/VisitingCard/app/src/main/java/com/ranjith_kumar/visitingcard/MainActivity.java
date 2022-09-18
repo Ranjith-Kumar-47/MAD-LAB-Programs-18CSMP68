52632c5b07c80335c3efa5972ddd63dbaa379d97
Ranjith-Kumar-47 <113786288+Ranjith-Kumar-47@users.noreply.github.com>
2022-09-18 22:46:51 +0530

    First Commit

---
+++ Progam-1-Visiting-Card/VisitingCard/app/src/main/java/com/ranjith_kumar/visitingcard/MainActivity.java
@@ -0,0 +1,12 @@
+package com.ranjith_kumar.visitingcard;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
